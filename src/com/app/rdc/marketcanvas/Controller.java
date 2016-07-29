a55249676482a55ca1526680f9e5bcd3c582c3c8
Ronald Partridge <rond@ronald-douglas.com>
2016-07-29 15:46:35 -0400

    Market canvas now its own application

---
+++ src/com/app/rdc/marketcanvas/Controller.java
@@ -0,0 +1,4 @@
+package com.app.rdc.marketcanvas;
+
+public class Controller {
+}
--- src/com/app/rdc/marketcanvas/Main.java
+++ src/com/app/rdc/marketcanvas/Main.java
@@ -1,4 +1,12 @@
-package sample;
+/**
+ * Market canvas display application
+ *
+ * @author  Ronald Partridge
+ * @version 0.1
+ * @since   2016-07-29
+ */
+
+package com.app.rdc.marketcanvas;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +18,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        Settings s = Settings.getInstance();
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle(s.APP_NAME);
+        primaryStage.setScene(new Scene(root, s.APP_WIDTH, s.APP_HEIGHT));
         primaryStage.show();
     }
 
